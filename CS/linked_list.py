60ac816e74bf848a355504fd6e62c739d86d9483
pinterarpad1977 <77687133+pinterarpad1977@users.noreply.github.com>
2024-02-27 21:42:32 +0100

    added nth_last_node method

--- CS/linked_list.py
+++ CS/linked_list.py
@@ -109,6 +109,31 @@ class LinkedList:
     node1.set_next_node(node2.get_next_node())
     node2.set_next_node(temp)
 
+  def nth_last_node(linked_list, n):
+    current = None
+    tail_seeker = linked_list.head_node
+    count = 1
+    while tail_seeker:
+      tail_seeker = tail_seeker.get_next_node()
+      count += 1
+      if count > n+1:
+        if current is None:
+          current = linked_list.head_node
+        else:
+          current = current.get_next_node()
+    return current
+  
+  def generate_test_linked_list():
+    linked_list = LinkedList()
+    for i in range(50, 0, -1):
+      linked_list.insert_beginning(i)
+    return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list()
+print(test_list.stringify_list())
+nth_last = nth_last_node(test_list, 4)
+print(nth_last.value)
 
 ll = LinkedList.LinkedList()
 for i in range(10):
