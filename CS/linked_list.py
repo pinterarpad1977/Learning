d90877de62a1254fa0111f03b339fb9a6c5b47b3
pinterarpad1977 <77687133+pinterarpad1977@users.noreply.github.com>
2024-02-27 17:49:03 +0100

    Create linked_list.py

---
+++ CS/linked_list.py
@@ -0,0 +1,78 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    try:
+      current_node = self.get_head_node()
+      if current_node.get_value() == value_to_remove:
+        self.head_node = current_node.get_next_node()
+      else:
+        while current_node:
+          next_node = current_node.get_next_node()
+          if next_node.get_value() == value_to_remove:
+            current_node.set_next_node(next_node.get_next_node())
+            current_node = None
+          else:
+            current_node = next_node
+    except AttributeError:
+      print(f'invalid value to remove -> {value_to_remove}')
+
+  def remove_all_nodes(self, value_to_remove):
+    try:
+      prev_node = None
+      current_node = self.get_head_node()
+      while current_node:
+        if current_node.get_value() == value_to_remove:
+          if prev_node:
+            prev_node.set_next_node(current_node.get_next_node())
+          else:
+            self.head_node = current_node.get_next_node()
+          current_node = current_node.get_next_node()
+        else:
+          prev_node = current_node
+          current_node = current_node.get_next_node()
+    except AttributeError:
+      print(f'invalid value to remove -> {value_to_remove}')
+
+ll = LinkedList("dog")
+ll.insert_beginning("cat")
+ll.insert_beginning("cat")
+ll.insert_beginning("cat")
+ll.insert_beginning("rat")
+ll.insert_beginning("lion")
+print(ll.stringify_list())
+ll.remove_all_nodes("cat")
+
+print(ll.stringify_list())
