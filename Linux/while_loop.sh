9eecd0aa73824a1fb7e4f6548b9f0510878536b5
pinterarpad1977 <77687133+pinterarpad1977@users.noreply.github.com>
2024-03-13 16:04:47 +0100

    while loop sh

---
+++ Linux/while_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+myvar=1
+while [ $myvar -le 10 ]
+do
+    echo $myvar
+    myvar=$(( $myvar + 1 ))
+    sleep 0.5
+done
+
+# -----------------------------
+
+while [ -f ~/testfile ]
+do
+    echo "As of $(date) The file exists"
+    sleep 5
+done
+
+echo "As of $(date) the file has gone missing. Exiting..."
