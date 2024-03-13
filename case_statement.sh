bf48011296ad0eba4b30408809b304c18e3c1f20
pinterarpad1977 <77687133+pinterarpad1977@users.noreply.github.com>
2024-03-13 19:09:51 +0100

    case example with while

--- case_statement.sh
+++ case_statement.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-echo "What is your favorite distro?"
+finished=0
+while [ $finished -ne 1 ]
+do
+    echo "What is your favorite distro?"
 
-echo "1 - Arch"
-echo "2 - CentOS"
-echo "3 - Debian"
-echo "4 - Mint"
-echo "5 - Ubuntu"
-echo "6 - Something else..."
+    echo "1 - Arch"
+    echo "2 - CentOS"
+    echo "3 - Debian"
+    echo "4 - Mint"
+    echo "5 - Ubuntu"
+    echo "6 - Something else..."
+    echo "7 - Exit the script"
 
-read distro;
+    read distro;
 
-case $distro in
-    1) "Arch is a rolling release.";;
-    2) "CentOS is popular on servers.";;
-    3) "Debian is a community distro.";;
-    4) "Mint is popular on desktops.";;
-    5) "Ubuntu is popular on both servers and desktops.";;
-    6) "So many distributions eh?";;
-    *) "invalid selection"
-esac
+    case $distro in
+        1) echo "Arch is a rolling release.";;
+        2) echo "CentOS is popular on servers.";;
+        3) echo "Debian is a community distro.";;
+        4) echo "Mint is popular on desktops.";;
+        5) echo "Ubuntu is popular on both servers and desktops.";;
+        6) echo "So many distributions eh?";;
+	7) finished=1;;
+        *) echo "invalid selection"
+    esac
+done
 
+echo "Thank you for using my script."
